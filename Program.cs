aba2c9cf0c24eb47cd5a5e7a736477bf0b6abda0
Tanjil <tanjilkh@gmail.com>
2024-05-26 20:19:54 +0100

    gtk initial experimental

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Gtk;
+
+namespace Weightconv
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            MainWindow win = new MainWindow();
+
+            // Label
+            Label label_lbs = new Label();
+            label_lbs.Text = "LBS";
+
+            win.Add(label_lbs);
+
+            win.ShowAll();
+            Application.Run();
+        }
+    }
+}
