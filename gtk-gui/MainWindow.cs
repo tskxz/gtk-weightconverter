3f78e6995546c439cdd47fa2ca95c0e64ddddd0b
Tanjil <tanjilkh@gmail.com>
2024-05-27 12:00:35 +0100

    label

--- gtk-gui/MainWindow.cs
+++ gtk-gui/MainWindow.cs
@@ -11,6 +11,8 @@ public partial class MainWindow
 
 	private global::Gtk.Entry entry2;
 
+	private global::Gtk.Label label1;
+
 	protected virtual void Build()
 	{
 		global::Stetic.Gui.Initialize(this);
@@ -19,8 +21,7 @@ public partial class MainWindow
 		this.Title = global::Mono.Unix.Catalog.GetString("Weightconv");
 		this.WindowPosition = ((global::Gtk.WindowPosition)(4));
 		// Container child MainWindow.Gtk.Container+ContainerChild
-		this.table1 = new global::Gtk.Table(((uint)(3)), ((uint)(3)), false);
-		this.table1.Name = "table1";
+		this.table1 = new global::Gtk.Table(((uint)(4)), ((uint)(3)), false);
 		this.table1.RowSpacing = ((uint)(6));
 		this.table1.ColumnSpacing = ((uint)(6));
 		// Container child table1.Gtk.Table+TableChild
@@ -31,8 +32,8 @@ public partial class MainWindow
 		this.button3.Label = global::Mono.Unix.Catalog.GetString("Convert");
 		this.table1.Add(this.button3);
 		global::Gtk.Table.TableChild w1 = ((global::Gtk.Table.TableChild)(this.table1[this.button3]));
-		w1.TopAttach = ((uint)(2));
-		w1.BottomAttach = ((uint)(3));
+		w1.TopAttach = ((uint)(3));
+		w1.BottomAttach = ((uint)(4));
 		w1.LeftAttach = ((uint)(1));
 		w1.RightAttach = ((uint)(2));
 		w1.XOptions = ((global::Gtk.AttachOptions)(4));
@@ -46,8 +47,8 @@ public partial class MainWindow
 		this.combobox1.Active = 0;
 		this.table1.Add(this.combobox1);
 		global::Gtk.Table.TableChild w2 = ((global::Gtk.Table.TableChild)(this.table1[this.combobox1]));
-		w2.TopAttach = ((uint)(1));
-		w2.BottomAttach = ((uint)(2));
+		w2.TopAttach = ((uint)(2));
+		w2.BottomAttach = ((uint)(3));
 		w2.LeftAttach = ((uint)(1));
 		w2.RightAttach = ((uint)(2));
 		w2.XOptions = ((global::Gtk.AttachOptions)(4));
@@ -60,17 +61,29 @@ public partial class MainWindow
 		this.entry2.InvisibleChar = '●';
 		this.table1.Add(this.entry2);
 		global::Gtk.Table.TableChild w3 = ((global::Gtk.Table.TableChild)(this.table1[this.entry2]));
+		w3.TopAttach = ((uint)(1));
+		w3.BottomAttach = ((uint)(2));
 		w3.LeftAttach = ((uint)(1));
 		w3.RightAttach = ((uint)(2));
 		w3.XOptions = ((global::Gtk.AttachOptions)(4));
 		w3.YOptions = ((global::Gtk.AttachOptions)(4));
+		// Container child table1.Gtk.Table+TableChild
+		this.label1 = new global::Gtk.Label();
+		this.label1.Name = "label1";
+		this.label1.LabelProp = global::Mono.Unix.Catalog.GetString("Weight: ");
+		this.table1.Add(this.label1);
+		global::Gtk.Table.TableChild w4 = ((global::Gtk.Table.TableChild)(this.table1[this.label1]));
+		w4.LeftAttach = ((uint)(1));
+		w4.RightAttach = ((uint)(2));
+		w4.XOptions = ((global::Gtk.AttachOptions)(4));
+		w4.YOptions = ((global::Gtk.AttachOptions)(4));
 		this.Add(this.table1);
 		if ((this.Child != null))
 		{
 			this.Child.ShowAll();
 		}
 		this.DefaultWidth = 400;
-		this.DefaultHeight = 300;
+		this.DefaultHeight = 146;
 		this.combobox1.HasDefault = true;
 		this.Show();
 		this.DeleteEvent += new global::Gtk.DeleteEventHandler(this.OnDeleteEvent);
