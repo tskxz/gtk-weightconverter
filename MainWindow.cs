2f33b4ed0e47a2255ad3c08acc3850f8c8a30af3
Tanjil <tanjilkh@gmail.com>
2024-05-27 18:24:49 +0100

    remove button convert

--- MainWindow.cs
+++ MainWindow.cs
@@ -36,6 +36,7 @@ public partial class MainWindow : Gtk.Window
             if (combobox1.ActiveText == "KG")
             {
                 Console.WriteLine("KG Selected");
+                label2.Text = "LBS";
 
                 float lbs_to_kg = (float)(i * 0.453);
                 Console.WriteLine("To KG: " + lbs_to_kg);
@@ -44,6 +45,7 @@ public partial class MainWindow : Gtk.Window
                 label1.Text = lbs_to_kg.ToString() + " KG";
             } else if (combobox1.ActiveText == "LBS")
             {
+                label2.Text = "KG";
                 float kg_to_lbs = (float)(i * 2.204);
                 label1.Text = kg_to_lbs.ToString() + " LBS";
             }
