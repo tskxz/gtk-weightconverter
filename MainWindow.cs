3b113e5213b6f09dcae4b6131e5c8a47068401bf
Tanjil <tanjilkh@gmail.com>
2024-05-28 10:33:13 +0100

    comments

--- MainWindow.cs
+++ MainWindow.cs
@@ -24,29 +24,36 @@ public partial class MainWindow : Gtk.Window
     protected void OnEntry2TextInserted(object o, TextInsertedArgs args)
     {
 
-        // Convert string to integer
+        // Convert entry text to number (float)
         float i;
         bool success = float.TryParse(entry2.Text, out i);
 
-        // Check if its really an integer
+        // Check if its really a number
         if (success)
         {
             Console.WriteLine(i);
-            // User wants to convert to KG
+            // User wants to convert that entry to KG
             if (combobox1.ActiveText == "KG")
             {
                 Console.WriteLine("KG Selected");
+
+                // Entry given is on LBS
                 label2.Text = "LBS";
 
                 float lbs_to_kg = (float)(i * 0.453);
                 Console.WriteLine("To KG: " + lbs_to_kg);
 
-                // Convert integer to string and display it in label
+                // Gets converted weight and display it
                 label1.Text = lbs_to_kg.ToString() + " KG";
-            } else if (combobox1.ActiveText == "LBS")
+            } 
+            // User wants to convert that entry to LBS
+            else if (combobox1.ActiveText == "LBS")
             {
+                // Entry given is on KG
                 label2.Text = "KG";
                 float kg_to_lbs = (float)(i * 2.204);
+
+                // Gets converted weight and display it
                 label1.Text = kg_to_lbs.ToString() + " LBS";
             }
         }
